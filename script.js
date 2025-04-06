82ce9bc0201f8c9c0598f4be7c618543c91d5330
Heder filho <hederfilho2050@gmail.com>
2025-04-05 22:24:39 -0300

    recordaçoes

--- script.js
+++ script.js
@@ -1,34 +1,83 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const carousel = document.querySelector('.carousel');
+let sliders = [];
 
-// Select tab content item
-function selectItem(e) {
-	// Remove all show and border classes
-	removeBorder();
-	removeShow();
-	// Add border to current tab item
-	this.classList.add('tab-border');
-	// Grab content item from DOM
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
-	// Add show class
-	tabContentItem.classList.add('show');
+let slideIndex = 0; // to track current slide index.
+
+ const createSlide = () => {
+    if(slideIndex >= movies.length){
+        slideIndex = 0;
+    } 
+    
+    // creating DOM element
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+    // attaching all elements
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+
+     // setting up image
+     imgElement.src = movies[slideIndex].image;
+     slideIndex++;
+
+     // setting elements classname
+     slide.className = 'slider';
+     content.className = 'slide-content';
+     h1.className = 'movie-title';
+     p.className = 'movie-des';
+ 
+     sliders.push(slide);
+
+// adding slider efects
+     if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
+    }
 }
+    for(let i = 0; i < 3; i++){
+        createSlide();
+    }
+    
+    setInterval(() => {createSlide();
+    }, 3000);
 
-// Remove bottom borders from all tab items
-function removeBorder() {
-	tabItems.forEach(item => {
-		item.classList.remove('tab-border');
-	});
-}
+    /// video cards
 
-// Remove show class from all content items
-function removeShow() {
-	tabContentItems.forEach(item => {
-		item.classList.remove('show');
-	});
-}
+const videoCards = [...document.querySelectorAll('.video-card')];
 
-// Listen for tab item click
-tabItems.forEach(item => {
-	item.addEventListener('click', selectItem);
-});
+videoCards.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        let video = item.children[1];
+        video.play();
+    })
+    item.addEventListener('mouseleave', () => {
+        let video = item.children[1];
+        video.pause();
+    })
+})
+// card sliders
+
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
